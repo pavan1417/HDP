890111b11545488e3c946ab09cdfbc88c6c595c6
pavan1417 <raatnalapavankumar007@gmail.com>
2025-04-04 15:21:13 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing 
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
